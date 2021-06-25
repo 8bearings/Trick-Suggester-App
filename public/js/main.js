ded7d754da71191183e104ff8f8146fb07d2131e
Elliott Watson <elliottwatson0@gmail.com>
2021-06-25 02:33:20 -0400

    feed updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,47 @@
 /* dropped in */
 
-$(window).on('resize', function() {
+// document.querySelectorAll(window).on('resize', function() {
 
-	var width = $(window).width();
-  var vert_pad = ['300px', '150px', '50px'];
-  var font_size_section = ['25px', '23px', '21px'];
-  var padding_socials = ['30px', '25px', '20px'];
-  var img_width_socials = ['30px', '28px', '26px'];
-  var i = 0;
+// 	var width = document.querySelector(window).width()
+//   var vert_pad = ['300px', '150px', '50px']
+//   var font_size_section = ['25px', '23px', '21px']
+//   var padding_socials = ['30px', '25px', '20px']
+//   var img_width_socials = ['30px', '28px', '26px']
+//   var i = 0
 
-	if (width <= 840) {
-  	i = 2;
-  } else if (width <= 1280) {
-  	i = 1;
-  } else {
-  	i = 0;
-  }
+// 	if (width <= 840) {
+//   	i = 2
+//   } else if (width <= 1280) {
+//   	i = 1
+//   } else {
+//   	i = 0
+//   }
   
-  $('.header').css('padding-left', vert_pad[i]);
-  $('.header').css('padding-right', vert_pad[i]);
+//   document.querySelector('.header').css('padding-left', vert_pad[i])
+//   document.querySelector('.header').css('padding-right', vert_pad[i])
   
-  $('.jumbo').css('padding-left', vert_pad[i]);
-  $('.jumbo').css('padding-right', vert_pad[i]);
+//   document.querySelector('.jumbo').css('padding-left', vert_pad[i])
+//   document.querySelector('.jumbo').css('padding-right', vert_pad[i])
   
-  $('.section').css('padding-left', vert_pad[i]);
-  $('.section').css('padding-right', vert_pad[i]);
-  $('.section').css('font-size', font_size_section[i]);
+//   document.querySelector('.section').css('padding-left', vert_pad[i])
+//   document.querySelector('.section').css('padding-right', vert_pad[i])
+//   document.querySelector('.section').css('font-size', font_size_section[i])
   
-  $('.image').css('padding-left', vert_pad[i]);
-  $('.image').css('padding-right', vert_pad[i]);
+//   document.querySelector('.image').css('padding-left', vert_pad[i])
+//   document.querySelector('.image').css('padding-right', vert_pad[i])
   
-  $('.socials').css('padding-left', vert_pad[i]);
-  $('.socials').css('padding-right', vert_pad[i]);
-  $('.socials a').css('padding-left', padding_socials[i]);
-  $('.socials a').css('padding-right', padding_socials[i]);
-  $('.socials a img').css('width', img_width_socials[i]);
+//   document.querySelector('.socials').css('padding-left', vert_pad[i])
+//   document.querySelector('.socials').css('padding-right', vert_pad[i])
+//   document.querySelector('.socials a').css('padding-left', padding_socials[i])
+//   document.querySelector('.socials a').css('padding-right', padding_socials[i])
+//   document.querySelector('.socials a img').css('width', img_width_socials[i])
   
-  $('.footer').css('padding-left', vert_pad[i]);
-  $('.footer').css('padding-right', vert_pad[i]);
+//   document.querySelector('.footer').css('padding-left', vert_pad[i])
+//   document.querySelector('.footer').css('padding-right', vert_pad[i])
 
-});
+// })
+
+// Feed Page
+
+
+
